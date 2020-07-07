0ad70f406a99d96040145c1460361e5c586f6d3d
Saulo Medeiros <saulo.medeiros@g4flex.com.br>
2020-07-07 14:14:18 -0300

    separating into modules and configuring imports

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,46 @@
+import { Router } from 'express';
+import multer from 'multer';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import uploadConfig from '@config/upload';
+
+const usersRouter = Router();
+
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+    const { name, email, password } = request.body;
+
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({
+        name,
+        email,
+        password,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+});
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        const updateUserAvatarService = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatarService.execute({
+            userId: request.user.id,
+            avatarFilename: request.file.originalname,
+        });
+
+        delete user.password;
+
+        return response.json(user);
+    }
+);
+
+export default usersRouter;
